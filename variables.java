e407925024230dad7dd8bd9d6945ca1dceec6539
cursed2024 <ashtondsouza289@gmail.com>
2026-01-20 12:24:42 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,12 @@
+class variables{
+    public static void main(String args[]){
+    int number = 524;
+    int age = 21;
+    float pi = 3.14f;
+    String FullName = "Ashton D'souza";
+    System.out.println("My name is " + FullName);
+    System.out.println("My age is " + age); 
+    System.out.println("The value of pi is " + pi);
+    System.out.println("My lucky number is " + number);
+    }
+}
