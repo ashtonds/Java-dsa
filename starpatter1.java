87e91871d42bfccf8c9c440a58d142ab6870396a
Ashton dsouza <ashtondsouza289@gmail.com>
2026-01-22 10:10:52 +0530

    initial

---
+++ starpatter1.java
@@ -0,0 +1,11 @@
+class starpatter1{
+    public static void main(String args[]){
+        int n=5;
+        for(int row=1; row<=n; row++){
+            for(int colomn=1; colomn<=n; colomn++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
