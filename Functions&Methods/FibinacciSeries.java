5e1134977643b2b13909131175bd62e25df5af22
Ashton dsouza <ashtondsouza289@gmail.com>
2026-01-24 12:37:33 +0530

    initial\

---
+++ Functions&Methods/FibinacciSeries.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class FibinacciSeries{
+    public static void fibonacci(int n){
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Series: ");
+        for(int i = 1; i <= n; i++){
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms in Fibonacci series: ");
+        int n = sc.nextInt();
+        fibonacci(n);
+        sc.close();
+    }
+}
