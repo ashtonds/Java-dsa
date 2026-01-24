7e5bfa00e2caae9938ade430c5c67b300bcaa449
Ashton dsouza <ashtondsouza289@gmail.com>
2026-01-24 11:48:33 +0530

    intial\

---
+++ Functions&Methods/infiniteLoop.java
@@ -0,0 +1,12 @@
+class infiniteLoop{
+
+
+    public static void main(String args[]){
+       int i = 0;
+        do {
+        System.out.println("This loop will run forever count :"+ i);
+        i++;
+        } while (true);
+
+    }
+}
