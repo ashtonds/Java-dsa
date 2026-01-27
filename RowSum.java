d8de2d4645412dc4286527f819b7071985375b7f
Ashton dsouza <ashtondsouza289@gmail.com>
2026-01-27 23:21:32 +0530

    initial

---
+++ RowSum.java
@@ -0,0 +1,16 @@
+public class RowSum {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        for (int i = 0; i < arr.length; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                rowSum += arr[i][j];
+            }
+            System.out.println("Row " + (i + 1) + " sum = " + rowSum);
+        }
+    }
+}
