54167dbd0dc62e44d2477516d6bcbfeb4aabe75a
Ashton dsouza <ashtondsouza289@gmail.com>
2026-01-27 23:21:53 +0530

    initial

---
+++ Transpose.java
@@ -0,0 +1,15 @@
+public class Transpose {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        for (int j = 0; j < arr[0].length; j++) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
